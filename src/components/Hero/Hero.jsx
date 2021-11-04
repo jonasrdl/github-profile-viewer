ec5f699b4dac272aca9eb31e346a52c4fe7cd9e3
Jonas <jonasriedel@protonmail.com>
2021-11-04 21:40:22 +0100

    Fix twitter not showing up if no username given

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Hero.css'
+import Link from '../Link/Link'
 const axios = require('axios').default
 const API = 'https://api.github.com'
 
@@ -39,37 +40,38 @@ class Hero extends React.Component {
   }
 
   render() {
-    console.log(this.state.usernameFromUrl)
-
     if (this.state.error === true) {
       return this.returnError()
     }
 
+    console.log(this.state.data)
+
     return (
-      <div className="main-container">
-        <img src={this.state.data.avatar_url} alt="Avatar" />
+      <div className="main">
+        <div>
+          <img src={this.state.data.avatar_url} alt="Avatar" />
+        </div>
         <p>Real name: {this.state.data.name || '/'}</p>
         <p>Username: {this.state.data.login || '/'}</p>
         <p>
-          Profile link:{' '}
-          <a href={this.state.data.html_url} target="_blank">
-            {this.state.data.html_url}
-          </a>
+          Profile link: <Link url={this.state.data.html_url} text={this.state.data.html_url} />
         </p>
         <p>Company: {this.state.data.company || '/'}</p>
         <p>
-          Website:{' '}
-          <a href={this.state.data.blog} target="_blank">
-            {this.state.data.blog}
-          </a>
+          Website: <Link url={this.state.data.blog} text={this.state.data.blog} />
         </p>
         <p>Location: {this.state.data.location || '/'}</p>
-        <p>
-          Twitter:{' '}
-          <a href={`https://twitter.com/${this.state.data.twitter_username}`} target="_blank">
-            {this.state.data.twitter_username}
-          </a>
-        </p>
+        {this.state.data.twitter_username === null ? (
+          <p>Twitter: /</p>
+        ) : (
+          <p>
+            Twitter:{' '}
+            <Link
+              url={`https://twitter.com/${this.state.data.twitter_username}`}
+              text={this.state.data.twitter_username}
+            />
+          </p>
+        )}
         <p>Email: {this.state.data.email || '/'}</p>
         <p>Followers: {this.state.data.followers || '/'}</p>
         <p>Following: {this.state.data.following || '/'}</p>

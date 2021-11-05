a83ffe591347c845fb811910e5048007b3a5000c
jonasrdl <jonasriedel@protonmail.com>
2021-11-05 07:52:28 +0100

    Change error text and styling

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Error.css'
 
 class Error extends React.Component {
   render() {
     return (
       <div className="error">
-        <h1>Error</h1>
-        <Link to={'/'}>Back home</Link>
+        <h1>An error occured</h1>
+        <Link to={'/'} className="link">
+          Back home
+        </Link>
       </div>
     )
   }
